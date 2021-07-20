670396c15aee75cc42493d3c6449471558b6f7c9
Weblet <shaun-007@hotmail.com>
2021-07-20 22:59:38 +0800

    Update project.metadata.cmake
    Changed project and display name.
    Changed website, discord and twitter links.
    Changed primary and secondary pre-loaded coins.
    Changed generated AppData folder name.

--- cmake/project.metadata.cmake
+++ cmake/project.metadata.cmake
@@ -1,14 +1,14 @@
 # Default project values
-set(DEX_PROJECT_NAME "atomicdex-desktop")
-set(DEX_DISPLAY_NAME "AtomicDEX Desktop")
+set(DEX_PROJECT_NAME "colliderdex-desktop")
+set(DEX_DISPLAY_NAME "ColliderDEX Desktop")
 set(DEX_MAINTENANCE_TOOL_NAME "AtomicDEX Maintenance Tool")
 set(DEX_COMPANY "KomodoPlatform")
-set(DEX_WEBSITE "https://atomicdex.io/")
+set(DEX_WEBSITE "https://cryptocollider.com/")
 set(DEX_SUPPORT_PAGE "https://support.komodoplatform.com/support/home")
-set(DEX_DISCORD "https://komodoplatform.com/discord")
-set(DEX_TWITTER "https://twitter.com/AtomicDEX")
-set(DEX_PRIMARY_COIN "KMD")                                                         ## Main coin of the DEX, will be enabled by default and will be the default left ticker for trading
-set(DEX_SECOND_PRIMARY_COIN "LTC")                                                  ## Second main coin of the DEX, will be enabled by default and will be the default right ticker for trading
+set(DEX_DISCORD "https://discord.com/invite/Uq9bpKN")
+set(DEX_TWITTER "https://twitter.com/crypto_collider")
+set(DEX_PRIMARY_COIN "CLC")                                                         ## Main coin of the DEX, will be enabled by default and will be the default left ticker for trading
+set(DEX_SECOND_PRIMARY_COIN "KMD")                                                  ## Second main coin of the DEX, will be enabled by default and will be the default right ticker for trading
 option(DISABLE_GEOBLOCKING "Enable to disable geoblocking (for dev purpose)" OFF)
 set(DEX_REPOSITORY_OWNER ${DEX_COMPANY})
 set(DEX_REPOSITORY_NAME "atomicDEX-Desktop")
@@ -16,7 +16,7 @@ set(DEX_CHECKSUM_API_URL "https://komodo.live/static/checksum.json")
 if (APPLE)
     set(DEX_APPDATA_FOLDER "AtomicDex Desktop")
 else ()
-    set(DEX_APPDATA_FOLDER "atomic_qt")
+    set(DEX_APPDATA_FOLDER "collider_dex")
 endif ()
 message(STATUS "APPDATA folder is ${DEX_APPDATA_FOLDER}")
 
